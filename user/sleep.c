9836f91432fee700da6a2a9872e369ab9f461563
willson <yuhangwei0517@163.com>
2024-12-20 16:18:28 +0800

    lab0 sleep4

--- user/sleep.c
+++ user/sleep.c
@@ -4,7 +4,7 @@
 
 int main(int argc, char *argv ){
     if (argc<=1){
-        print("erro!please wirte the seconds you want sleep for" );
+        printf("erro!please wirte the seconds you want sleep for" );
         exit(1);
     }
     sleep(atoi(argv[1]));
