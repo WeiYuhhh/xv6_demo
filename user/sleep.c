641ee053a27837ad922b80c6df471ac2f3d45bfd
willson <yuhangwei0517@163.com>
2024-12-20 16:31:31 +0800

    lab0 sleep5

--- user/sleep.c
+++ user/sleep.c
@@ -2,7 +2,7 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int main(int argc, char *argv ){
+int main(int argc, char *argv[]){
     if (argc<=1){
         printf("erro!please wirte the seconds you want sleep for" );
         exit(1);
